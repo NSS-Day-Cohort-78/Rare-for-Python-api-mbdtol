5c1606ab28644539422c7bbba46961d5318b5d60
James Heaton <heaton1138@gmail.com>
2025-10-23 16:00:52 -0500

    View Profile Added

--- views/__init__.py
+++ views/__init__.py
@@ -1,2 +1,2 @@
-from .user import login_user, create_user, get_posts_by_user, get_all_posts
+from .user import login_user, create_user, get_posts_by_user, get_all_posts, get_user_by_id
 from .post import get_post_by_id, get_categories, update_post, create_post, delete_post
